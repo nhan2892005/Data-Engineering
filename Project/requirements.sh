b146026771b934ead06086186ff80c1e9b372baa
nhan2892005 <143471539+nhan2892005@users.noreply.github.com>
2025-05-31 12:55:29 +0700

    Begin project

---
+++ Project/requirements.sh
@@ -0,0 +1,39 @@
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y curl git apt-transport-https ca-certificates gnupg lsb-release
+
+sudo apt install -y openjdk-11-jdk
+java -version
+
+# add sbt repository
+echo "deb https://repo.scala-sbt.org/scalasbt/debian all main" \
+  | sudo tee /etc/apt/sources.list.d/sbt.list
+curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x99E82A75642AC823" \
+  | sudo apt-key add -
+
+sudo apt update
+sudo apt install -y sbt
+sbt sbtVersion
+
+# add Docker’s GPG key and repo
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg \
+  | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] \
+  https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" \
+  | sudo tee /etc/apt/sources.list.d/docker.list
+
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo groupadd --system docker      # if not already present
+sudo usermod -aG docker $USER
+docker run hello-world
+
+sudo apt install -y docker-compose-plugin
+docker compose version
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" \
+  -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
